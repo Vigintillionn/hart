a1f4e45eca6939b4bc763b30f85f8a016f809811
Vigintillionn <yarne.ramakers@gmail.com>
2026-01-08 22:02:37 +0100

    feat: implement assembleIType

--- app/Assembler.hs
+++ app/Assembler.hs
@@ -8,7 +8,7 @@ import Data.Bits (Bits(shiftL, (.|.)))
 getRFmt :: ROp -> (Word32, Word32, Word32)
 getRFmt op = (opc, f3, f7)
     where
-        opc = 0x67
+        opc = 0x33
         f3  = case op of
             XOR -> 0x4
             _   -> 0x0
@@ -30,9 +30,29 @@ assembleRType (RType op args) =
        rs1  = fromIntegral $ r_rs1 args
        rs2  = fromIntegral $ r_rs2 args 
 
+getIFmt :: IOp -> (Word32, Word32)
+getIFmt op = (opc, f3)
+    where
+        opc = 0x13
+        f3  = case op of
+            _   -> 0x0
+
 assembleIType :: Instruction 'I -> Word32
-assembleIType = undefined
+assembleIType (IType op args) =
+    opc .|.
+    (rd `shiftL` 7) .|.
+    (f3 `shiftL` 12) .|.
+    (rs1 `shiftL` 15) .|.
+    (imm `shiftL` 20)
+    where
+        (opc, f3)   = getIFmt op
+        rd          = fromIntegral $ i_rd args
+        rs1         = fromIntegral $ i_rs1 args
+        imm         = fromIntegral $ i_imm args
 
 assembleSome :: SomeInstruction -> Word32
 assembleSome (SomeInstruction instr@(RType _ _)) = assembleRType instr
 assembleSome (SomeInstruction instr@(IType _ _)) = assembleIType instr
+
+assemble :: [SomeInstruction] -> [Word32]
+assemble = map assembleSome
