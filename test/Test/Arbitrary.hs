89c0790188d1e8eb0c778d20e2efcd5f6c508aa1
Vigintillionn <yarne.ramakers@gmail.com>
2026-01-21 11:19:47 +0100

    feat: add unit tests

---
+++ test/Test/Arbitrary.hs
@@ -0,0 +1,69 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+module Test.Arbitrary where
+
+import Test.QuickCheck
+import Types
+
+mkReg :: Int -> Register
+mkReg n = case mkRegister n of
+    Just r -> r
+    Nothing -> error $ "Bad Test Reg: " ++ show n
+
+instance Arbitrary Register where
+    arbitrary = do
+        n <- choose (0, 31)
+        return $ mkReg n
+
+instance Arbitrary ROp where
+    arbitrary = elements [ADD, SUB, XOR, OR, AND]
+
+instance Arbitrary IArithOp where
+    arbitrary = elements [ADDI, XORI, ORI, ANDI]
+
+instance Arbitrary ILoadOp where
+    arbitrary = elements [LB, LH, LW]
+
+instance Arbitrary BOp where
+    arbitrary = elements [BEQ, BNE, BLT, BGE]
+
+instance Arbitrary SOp where
+    arbitrary = elements [SB, SH, SW]
+
+genImm :: Int -> Gen Int
+genImm bits = choose (-(2^(bits-1)), 2^(bits-1) - 1)
+
+instance Arbitrary (SomeInstruction Int) where
+    arbitrary = oneof
+        [ genRType
+        , genITypeArith
+        , genITypeLoad
+        , genBType
+        , genSType
+        ]
+      where
+        genRType = do
+            op <- arbitrary
+            args <- RTypeArgs <$> arbitrary <*> arbitrary <*> arbitrary
+            return $ SomeInstruction (RType op args)
+
+        genITypeArith = do
+            op <- arbitrary
+            args <- ITypeArgs <$> arbitrary <*> arbitrary <*> genImm 12
+            return $ SomeInstruction (ArithI op args)
+
+        genITypeLoad = do
+            op <- arbitrary
+            args <- ITypeArgs <$> arbitrary <*> arbitrary <*> genImm 12
+            return $ SomeInstruction (LoadI op args)
+
+        genBType = do
+            op <- arbitrary
+            imm <- (*2) <$> genImm 12 -- Ensure alignment
+            args <- BTypeArgs <$> arbitrary <*> arbitrary <*> pure imm
+            return $ SomeInstruction (BType op args)
+
+        genSType = do
+            op <- arbitrary
+            imm <- genImm 12
+            args <- STypeArgs <$> arbitrary <*> arbitrary <*> pure imm
+            return $ SomeInstruction (SType op args)
---
+++ test/Test/AssemblerSpec.hs
@@ -0,0 +1,18 @@
+module Test.AssemblerSpec (spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+import Test.Arbitrary ()
+import Assembler
+import Decoder
+
+spec :: Spec
+spec = do
+    describe "Assembler - Decoder" $ do
+        it "decoding assembled instruction returns original instruction" $
+            withMaxSuccess 10000 $ property $ \instr ->
+                let machineCode = assembleSome instr
+                    decoded     = decodeWord machineCode
+                in case decoded of
+                    Right res -> res `shouldBe` instr
+                    Left err  -> expectationFailure $ "Decode failed: " ++ err
