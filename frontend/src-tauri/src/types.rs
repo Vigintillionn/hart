f763dfc0357001fa73d6a60b693fb74923f01633
Vigintillionn <yarne.ramakers@gmail.com>
2026-05-28 22:40:19 +0200

    chore: use cpustatus enum instead of strings

--- frontend/src-tauri/src/types.rs
+++ frontend/src-tauri/src/types.rs
@@ -1,13 +1,22 @@
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
 
+#[derive(Serialize, Deserialize, TS, Clone, Debug, PartialEq)]
+#[ts(export, export_to = "../src/bindings/")]
+pub enum CpuStatus {
+    Running,
+    Halted,
+    Paused,
+    WaitingForInput,
+}
+
 #[derive(Serialize, Deserialize, TS, Clone, Debug)]
 #[ts(export, export_to = "../src/bindings/")]
 pub struct CpuState {
     pub pc: u32,
     pub regs: Vec<i32>,
     pub cycles: usize,
-    pub status: String,
+    pub status: CpuStatus,
     pub csrs: Vec<(u32, u32)>,
     pub mem: Vec<(u64, u8)>,
     #[serde(rename = "heapTop")]
