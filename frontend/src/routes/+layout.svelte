1a0b408730660fbe01e2492ef4b65227a333ebaf
Vigintillionn <yarne.ramakers@gmail.com>
2026-05-31 18:15:14 +0200

    fix: layout component is now typed

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,8 @@
-<script>
+<script lang="ts">
+  import type { Snippet } from "svelte";
   import "../app.css";
   import Titlebar from "../components/Titlebar.svelte";
-  let { children } = $props();
+  let { children }: { children: Snippet } = $props();
 </script>
 
 <div
