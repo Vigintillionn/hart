080c5d76efe967d93b6a06026cf3b66ead8e1994
Vigintillionn <yarne.ramakers@gmail.com>
2026-06-04 01:50:48 +0200

    chore: fix deprecated code

--- emulator/test/Test/AssemblerSpec.hs
+++ emulator/test/Test/AssemblerSpec.hs
@@ -1,19 +1,20 @@
 module Test.AssemblerSpec (spec) where
 
-import Test.Hspec
-import Test.QuickCheck
-import Test.Arbitrary ()
 import Assembler
 import Decoder
 import Extension (allExtensions)
+import Test.Arbitrary ()
+import Test.Hspec
+import Test.QuickCheck
 
 spec :: Spec
 spec = do
-    describe "Assembler - Decoder" $ do
-        it "decoding assembled instruction returns original instruction" $
-            withMaxSuccess 10000 $ property $ \instr ->
-                let machineCode = assembleSome instr
-                    decoded     = decodeWord allExtensions machineCode
-                in case decoded of
-                    Right res -> res `shouldBe` instr
-                    Left err  -> expectationFailure $ "Decode failed: " ++ show err
+  describe "Assembler - Decoder" $ do
+    it "decoding assembled instruction returns original instruction" $
+      withNumTests 10000 $
+        property $ \instr ->
+          let machineCode = assembleSome instr
+              decoded = decodeWord allExtensions machineCode
+           in case decoded of
+                Right res -> res `shouldBe` instr
+                Left err -> expectationFailure $ "Decode failed: " ++ show err
